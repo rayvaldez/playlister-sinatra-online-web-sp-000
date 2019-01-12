9f564707c82423d7b4f65cede959077b51c4271f
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-01-12 01:36:52 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,7 +22,7 @@ class SongsController < ApplicationController
     @song.genre_ids = params[:genres]
 
     if @song.save
-      redirect :"songs/#{@song.slug}"
+      redirect :"songs/#{@song.slug}", :locals {message: "Successfully created song."}
     else
       erb :"songs/show", locals: {message: "Successfully created song."}
     end
