6b16427e44b840bd9dee604b6608f68bd9290ce5
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-01-12 01:43:05 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,7 +22,7 @@ class SongsController < ApplicationController
     @song.genre_ids = params[:genres]
     @song.save
 
-    erb :"songs/show", :locals {message: "Successfully created song."}
+    erb :"songs/show", locals: {message: "Successfully created song."}
   end
 
   get '/songs/:slug/edit' do
