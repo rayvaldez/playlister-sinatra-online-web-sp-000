2b5858e4673c3fb4a88462a28e748abbb171db09
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-01-12 00:32:58 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,6 +28,15 @@ class SongsController < ApplicationController
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
 
-    erb :'songs/edit', locals: {message: "Successfully updated song."}
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+
+    erb :'songs/show', , locals: {message: "Successfully updated song."}
   end
 end
